8380e35358532d903db6314d3da471f53f9ba2c6
Aadi Jain <aadijaintikamgarh@gmail.com>
2025-06-14 17:22:16 +0530

    add middleware.js

---
+++ src/middleware.js
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request) {
+    const path = request.nextUrl.pathname;
+    
+    const isPublicPath = path == "/login" || path == "/signup";
+
+    const token = request.cookies.get("token")?.value || ""
+
+    if(isPublicPath && token){
+        const data = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET)
+        return NextResponse.redirect(new URL("/", request.url)).json({user: data._id})
+    }
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ["/","/signup","/login"]
+};
