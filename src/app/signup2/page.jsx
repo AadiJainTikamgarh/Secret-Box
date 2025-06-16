7ea9f83957c0f815641fded14bc5602a3c1267b8
Saumya khushlani <saumyakhushlani9@gmail.com>
2025-06-17 01:24:17 +0530

    Fixing sign up form

--- src/app/signup2/page.jsx
+++ src/app/signup2/page.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Form from '@/Components/Form'
 import Header from '@/Components/Header'
 import Footer from '@/Components/Footer'
+import LoginForm from '@/Components/LoginForm'
 
 const page = () => {
   return (
     <div className='min-h-screen'>
      <Header/>
     <div className='flex flex-col justify-center items-center px-4 py-10 bg-gradient-to-br from-[#4212de30] to-[#4212de05] min-h-[80vh]'>
-      <Form/>
+      <LoginForm/>
     </div>
     <Footer/>
     </div>
