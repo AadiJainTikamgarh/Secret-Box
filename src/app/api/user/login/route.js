961e3ce82c7a002b58f77e751c47fe20fe259bbe
Aadi Jain <aadijaintikamgarh@gmail.com>
2025-06-18 03:12:25 +0530

    add google auth

--- src/app/api/user/login/route.js
+++ src/app/api/user/login/route.js
@@ -4,73 +4,75 @@ import mongoose from "mongoose";
 import { User } from "@/models/userModels";
 
 export async function POST(request) {
-    try {
-        await dbConnect();
+  try {
+    await dbConnect();
+    const body = await request.json();
+    const { email, password, withGoogle } = body;
 
-        const body = await request.json();
-        const { email, password } = body
-
-        if (!email || !password) {
-            return NextResponse.json(
-                { error: "Please Provide Both Details" },
-                { status: 403 }
-            )
-        }
-
-        console.log("Login request:", { email, password });
-        const user = await User.findOne({ email })
-        if (!user) {
-            return NextResponse.json(
-                { error: "No User found with this email. Try to SignUp first." },
-                { status: 404 }
-            )
-        }
-
-
-        //password checking
-        const isPasswordValid = await user.isPasswordMatch(password);
-
-
-
-
-        if (!isPasswordValid) {
-            return NextResponse.json(
-                { error: "Invalid password" },
-                { status: 401 }
-            );
-        }
-
-
-        //Adding and saving the refresh token
-        const refreshToken = user.generateRefreshToken();
-        user.refreshToken = refreshToken;
-        await user.save();
-
-        const userResponse = {
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            photo: user.photo,
-            nglToken: user.nglToken
-        };
-
-        
-        const response = NextResponse.json(
-            {
-                message: "Login successful",
-                user: userResponse,
-                refreshToken
-            },
-            { status: 200 }
-        );
-        response.cookies.set("token", refreshToken, { httpOnly: true })
-
-        return response
-
-    } catch (error) {
+    let user = undefined;
+    if (!withGoogle) {
+      if (!email || !password) {
         return NextResponse.json(
-            { error: error.message || "Internal Server Error" },
-            { status: 500 }
-        )
+          { error: "Please Provide Both Details" },
+          { status: 403 }
+        );
+      }
+
+      console.log("Login request:", { email, password });
+      user = await User.findOne({ email });
+      if (!user) {
+        return NextResponse.json(
+          { error: "No User found with this email. Try to SignUp first." },
+          { status: 404 }
+        );
+      }
+
+      //password checking
+      const isPasswordValid = await user.isPasswordMatch(password);
+
+      if (!isPasswordValid) {
+        return NextResponse.json(
+          { error: "Invalid password" },
+          { status: 401 }
+        );
+      }
+    } else {
+      user = await User.findOne({ email });
+      if (!user) {
+        user = await User({
+          email: email,
+        });
+      }
     }
+
+    //Adding and saving the refresh token
+    const refreshToken = user.generateRefreshToken();
+    user.refreshToken = refreshToken;
+    await user.save();
+
+    const userResponse = {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      photo: user.photo,
+      nglToken: user.nglToken,
+    };
+
+    const response = NextResponse.json(
+      {
+        message: "Login successful",
+        user: userResponse,
+        refreshToken,
+      },
+      { status: 200 }
+    );
+    response.cookies.set("token", refreshToken, { httpOnly: true });
+
+    return response;
+  } catch (error) {
+    return NextResponse.json(
+      { error: error.message || "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import {SessionWrapper} from "@/Wrapper/SessionWrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,13 +20,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-       
+      <SessionWrapper>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
           {children}
-        
-      </body>
+        </body>
+      </SessionWrapper>
     </html>
   );
 }
